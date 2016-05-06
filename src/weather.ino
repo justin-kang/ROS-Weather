16bf25a9368b5bdb38060e040d8bd54e1a00d2b3
Justin Kang <justin.kang23@gmail.com>
2016-05-06 14:51:29 -0500

    Attempted bufxies

--- src/weather.ino
+++ src/weather.ino
@@ -7,11 +7,12 @@ rgb_color colors[LED_COUNT];
 String temp;
 int temps[20];
 int count = 0;
+int set = 0;
 
 void setup() {
     Serial.begin(9600);
-    for (int j = 0; j < 20; j++) {
-        temps[j] = 0;
+    for (int i = 0; i < 20; i++) {
+        temps[i] = 0;
     }
     while (!Serial);
 }
@@ -20,33 +21,35 @@ void setTemps() {
     if (count >= 20)
         return;
     while (!Serial.available());
-    while (Serial.available()) {
-        if (Serial.available() > 0) {
-            char c = Serial.read();;
+    while (Serial.available() > 0) {
+        char c = Serial.read();
+        if (c == ' ') {
+            if (temp.length() != 0) {
+                Serial.print(temp);
+                double t = (double)temp.toInt();
+                t = t / 120.0 * 255.0;
+                if (t > 255.0)
+                    t = 255.0;
+                temps[count] = (int)t;
+                count++;
+            }
+            temp = "";
+            continue;
         }
+        temp += c;
     }
-    double t = (double)temp.toInt();
-    t = t / 120.0 * 255.0;
-    temps[count] = (int)t;
-    #if (temp.length() > 0) {
-    #    Serial.print("Arduino received ");
-    #    Serial.println(temp);
-    #}
-    #delay(500);
-    #char ard_sends = '1';
-    #Serial.print("Adruino sends: ");
-    #Serial.println(ard_sends);
-    #Serial.print("\n");
-    #Serial.flush();
 }
 
 void loop() {
+    if (set == 1)
+        return;
     setTemps();
     for (int i = 0; i < 20; i++) {
         colors[3*i+0] = (rgb_color){255, 255-temps[i], 0};
         colors[3*i+1] = (rgb_color){255, 255-temps[i], 0};
         colors[3*i+2] = (rgb_color){255, 255-temps[i], 0};
     }
-    ledStrip.write(colors, LED_COUNT)
+    ledStrip.write(colors, LED_COUNT);
+    set = 1;
     delay(20);
 }
--- src/weather.py
+++ src/weather.py
@@ -32,53 +32,12 @@ for h in range(0,20):
         temp_dict[h] = -99
 
 '''
-port = "dev/ttyACM0"
+#send temperatures to arduino
+port = "/dev/ttyACM0"
 ard = serial.Serial(port, 9600, timeout = 5)
 
 for i in range(0, 20):
-    ard.flush()
-    ard.write(temp_dict[i])
+    #ard.flush()
+    ard.write(temp_dict[i] + ' ')
     time.sleep(1)
-'''
-
-'''
-#print temperatures to file "temps.txt"
-file = open("temps.txt", 'w')
-for i in range(0, 24):
-    t = str(temp_dict[i])+'\n'
-    file.write(t)
-
-#get current time
-current_time = time.localtime()
-current_hour = current_time.tm_hour
-month = current_time.tm_mon
-day = current_time.tm_mday
-year = current_time.tm_year
-
-#formatting
-counter = 1
-am = True
-degree = u"\N{DEGREE SIGN}"'F'
-
-#print out temperature at each hour
-for i in range(0,24):
-    hour = (current_hour + counter) % 12
-    if (hour == 0):
-        hour = 12
-    am = (current_hour + counter) / 12
-    if (am % 2 == 0):
-        am = True
-    else:
-        am = False
-    if ((hour == 12) & (am == True)):
-        day = day + 1
-    print("At time", hour, end="")
-    if am == True:
-        print("am, ", end="")
-    else:
-        print("pm, ", end="")
-    print("{m}/{d}/{y}, ".format(m=month, d=day, y=year), end="")
-    t = str(temp_dict[i])+degree
-    print("temperature is", t, end='\n')
-    counter = counter + 1
 '''
