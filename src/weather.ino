3ebcd0360344ff59823a441dd14056abb2911c93
Justin Kang <justin.kang23@gmail.com>
2016-05-06 13:35:35 -0500

    pretest

--- src/weather.ino
+++ src/weather.ino
@@ -22,31 +22,30 @@ void setTemps() {
     while (!Serial.available());
     while (Serial.available()) {
         if (Serial.available() > 0) {
-            char c = Serial.read();
-            temp += c;
+            char c = Serial.read();;
         }
     }
     double t = (double)temp.toInt();
     t = t / 120.0 * 255.0;
     temps[count] = (int)t;
-    if (temp.length() > 0) {
-        Serial.print("Arduino received ");
-        Serial.println(temp);
-    }
-    delay(500);
-    char ard_sends = '1';
-    Serial.print("Adruino sends: ");
-    Serial.println(ard_sends);
-    Serial.print("\n");
-    Serial.flush();
+    #if (temp.length() > 0) {
+    #    Serial.print("Arduino received ");
+    #    Serial.println(temp);
+    #}
+    #delay(500);
+    #char ard_sends = '1';
+    #Serial.print("Adruino sends: ");
+    #Serial.println(ard_sends);
+    #Serial.print("\n");
+    #Serial.flush();
 }
 
 void loop() {
     setTemps();
     for (int i = 0; i < 20; i++) {
-        colors[3*i+0] = temps[i];
-        colors[3*i+1] = temps[i];
-        colors[3*i+2] = temps[i];
+        colors[3*i+0] = (rgb_color){255, 255-temps[i], 0};
+        colors[3*i+1] = (rgb_color){255, 255-temps[i], 0};
+        colors[3*i+2] = (rgb_color){255, 255-temps[i], 0};
     }
     ledStrip.write(colors, LED_COUNT)
     delay(20);
--- src/weather.py
+++ src/weather.py
@@ -8,7 +8,7 @@ http://stackoverflow.com/questions/24074914/python-to-arduino-serial-read-write
 
 from __future__ import print_function
 import json
-import rospy
+#import rospy
 import serial
 import time
 import urllib2
@@ -24,12 +24,14 @@ parsed_json = json.loads(json_string)
 
 #store temperatures in dictionary
 temp_dict = {}
-for h in range(0,24):
+for h in range(0,20):
     try:
         temp_dict[h] = parsed_json["hourly_forecast"][h]["temp"]["english"]
+        print(temp_dict[h])
     except IndexError:
         temp_dict[h] = -99
 
+'''
 port = "dev/ttyACM0"
 ard = serial.Serial(port, 9600, timeout = 5)
 
@@ -37,6 +39,7 @@ for i in range(0, 20):
     ard.flush()
     ard.write(temp_dict[i])
     time.sleep(1)
+'''
 
 '''
 #print temperatures to file "temps.txt"
